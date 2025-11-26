6aa0d767605ddbd27e81c585fe2aae372f09dcd2
Viktor Ferenczi <viktor@ferenczi.eu>
2025-11-26 23:58:55 +0100

    Removed AssemblyInfo

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -13,101 +13,108 @@ using VRage.FileSystem;
 using VRage.Game;
 using VRage.Plugins;
 
-namespace ClientPlugin
-{
-    // ReSharper disable once UnusedType.Global
-    public class Plugin : IPlugin, ICommonPlugin
-    {
-        public const string Name = "PluginTemplate";
-        public static Plugin Instance { get; private set; }
-        private SettingsGenerator settingsGenerator;
-        public long Tick { get; private set; }
-        private static bool failed;
+// Set the assembly version manually if compiled by Pulsar (it won't create what was in AssemblyInfo.cs before)
+#if !DEV_BUILD
+using System.Reflection;
 
-        public IPluginLogger Log => Logger;
-        private static readonly IPluginLogger Logger = new PluginLogger(Name);
-
-        public IPluginConfig Config => config?.Data;
-        private PersistentConfig<PluginConfig> config;
-        private static readonly string ConfigFileName = $"{Name}.cfg";
-
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        public void Init(object gameInstance)
-        {
-#if DEBUG
-            // Allow the debugger some time to connect once the plugin assembly is loaded
-            Thread.Sleep(100);
+[assembly: AssemblyVersion("1.11.18.0")]
+[assembly: AssemblyFileVersion("1.11.18.0")]
 #endif
 
-            Instance = this;
-            Instance.settingsGenerator = new SettingsGenerator();
+namespace ClientPlugin;
 
-            Log.Info("Loading");
+// ReSharper disable once UnusedType.Global
+public class Plugin : IPlugin, ICommonPlugin
+{
+    public const string Name = "PluginTemplate";
+    public static Plugin Instance { get; private set; }
+    private SettingsGenerator settingsGenerator;
+    public long Tick { get; private set; }
+    private static bool failed;
 
-            var configPath = Path.Combine(MyFileSystem.UserDataPath, ConfigFileName);
-            config = PersistentConfig<PluginConfig>.Load(Log, configPath);
+    public IPluginLogger Log => Logger;
+    private static readonly IPluginLogger Logger = new PluginLogger(Name);
 
-            var gameVersion = MyFinalBuildConstants.APP_VERSION_STRING.ToString();
-            Common.SetPlugin(this, gameVersion, MyFileSystem.UserDataPath);
+    public IPluginConfig Config => config?.Data;
+    private PersistentConfig<PluginConfig> config;
+    private static readonly string ConfigFileName = $"{Name}.cfg";
 
-            if (!PatchHelpers.HarmonyPatchAll(Log, new Harmony(Name)))
-            {
-                failed = true;
-                return;
-            }
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+    public void Init(object gameInstance)
+    {
+#if DEBUG
+        // Allow the debugger some time to connect once the plugin assembly is loaded
+        Thread.Sleep(100);
+#endif
 
-            Log.Debug("Successfully loaded");
+        Instance = this;
+        Instance.settingsGenerator = new SettingsGenerator();
+
+        Log.Info("Loading");
+
+        var configPath = Path.Combine(MyFileSystem.UserDataPath, ConfigFileName);
+        config = PersistentConfig<PluginConfig>.Load(Log, configPath);
+
+        var gameVersion = MyFinalBuildConstants.APP_VERSION_STRING.ToString();
+        Common.SetPlugin(this, gameVersion, MyFileSystem.UserDataPath);
+
+        if (!PatchHelpers.HarmonyPatchAll(Log, new Harmony(Name)))
+        {
+            failed = true;
+            return;
         }
 
-        public void Dispose()
-        {
-            try
-            {
-                // TODO: Save state and close resources here, called when the game exists (not guaranteed!)
-                // IMPORTANT: Do NOT call harmony.UnpatchAll() here! It may break other plugins.
-            }
-            catch (Exception ex)
-            {
-                Log.Critical(ex, "Dispose failed");
-            }
-
-            Instance = null;
-        }
-
-        public void Update()
-        {
-            if (failed)
-                return;
-
-            try
-            {
-                CustomUpdate();
-                Tick++;
-            }
-            catch (Exception ex)
-            {
-                Log.Critical(ex, "Update failed");
-                failed = true;
-            }
-        }
-
-        private void CustomUpdate()
-        {
-            // TODO: Put your update code here. It is called on every simulation frame!
-            PatchHelpers.PatchUpdates();
-        }
-
-        // ReSharper disable once UnusedMember.Global
-        public void OpenConfigDialog()
-        {
-            Instance.settingsGenerator.SetLayout<Simple>();
-            MyGuiSandbox.AddScreen(Instance.settingsGenerator.Dialog);
-        }
-        
-        //TODO: Uncomment and use this method to load asset files
-        /*public void LoadAssets(string folder)
-        {
-
-        }*/
+        Log.Debug("Successfully loaded");
     }
+
+    public void Dispose()
+    {
+        try
+        {
+            // TODO: Save state and close resources here, called when the game exists (not guaranteed!)
+            // IMPORTANT: Do NOT call harmony.UnpatchAll() here! It may break other plugins.
+        }
+        catch (Exception ex)
+        {
+            Log.Critical(ex, "Dispose failed");
+        }
+
+        Instance = null;
+    }
+
+    public void Update()
+    {
+        if (failed)
+            return;
+
+        try
+        {
+            CustomUpdate();
+            Tick++;
+        }
+        catch (Exception ex)
+        {
+            Log.Critical(ex, "Update failed");
+            failed = true;
+        }
+    }
+
+    private void CustomUpdate()
+    {
+        // TODO: Put your update code here. It is called on every simulation frame!
+        PatchHelpers.PatchUpdates();
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public void OpenConfigDialog()
+    {
+        Instance.settingsGenerator.SetLayout<Simple>();
+        MyGuiSandbox.AddScreen(Instance.settingsGenerator.Dialog);
+    }
+        
+    //TODO: Uncomment and use this method to load asset files
+    /*public void LoadAssets(string folder)
+    {
+
+    }*/
 }
