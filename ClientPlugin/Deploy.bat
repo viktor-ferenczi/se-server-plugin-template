b5110cab268a7d40ea7f8a7527e30f48873697cf
Viktor Ferenczi <viktor@ferenczi.eu>
2025-11-27 00:01:48 +0100

    Fixed deployment

--- ClientPlugin/Deploy.bat
+++ ClientPlugin/Deploy.bat
@@ -1,9 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Check if the required parameters are passed
-REM (3rd param will be blank if there are not enough)
-if "%~3" == "" (
+REM Check if the required parameters are passed (NAME and SOURCE)
+if "%~2" == "" (
     echo ERROR: Missing required parameters
     exit /b 1
 )
@@ -11,23 +10,35 @@ if "%~3" == "" (
 REM Extract parameters and remove quotes
 set NAME=%~1
 set SOURCE=%~2
-set BIN64=%~3
+
+REM Resolve source file:
+REM - If "%SOURCE%\%NAME%" exists, use that
+REM - Else if "%SOURCE%" exists and is a file, use it
+REM - Else fail
+set "SRCFILE="
+if exist "%SOURCE%\%NAME%" (
+    set "SRCFILE=%SOURCE%\%NAME%"
+) else if exist "%SOURCE%" (
+    set "SRCFILE=%SOURCE%"
+) else (
+    echo ERROR: Source not found: %SOURCE% or %SOURCE%\%NAME%
+    exit /b 1
+)
 
 REM Remove trailing backslash if applicable
 if "%NAME:~-1%"=="\" set NAME=%NAME:~0,-1%
 if "%SOURCE:~-1%"=="\" set SOURCE=%SOURCE:~0,-1%
-if "%BIN64:~-1%"=="\" set BIN64=%BIN64:~0,-1%
 
 REM Verify Pulsar deployment and Local plugin folder
 set PLUGIN_DIR=%AppData%\Pulsar\Legacy\Local
 if not exist "%PLUGIN_DIR%" (
-    echo "Missing Local plugin folder: %PLUGIN_DIR%"  
+    echo "Missing Local plugin folder: %PLUGIN_DIR%"
     echo "Pulsar not installed?"
     exit /b 2
 )
 
 REM Copy the plugin into the plugin directory
-echo Copying "%NAME%" to "%PLUGIN_DIR%\"
+echo Copying "%SOURCE%\%NAME%" to "%PLUGIN_DIR%\"
 
 for /l %%i in (1, 1, 10) do (
     copy /y "%SOURCE%\%NAME%" "%PLUGIN_DIR%\"
--- TorchPlugin/Deploy.bat
+++ TorchPlugin/Deploy.bat
@@ -9,40 +9,30 @@ if "%~3" == "" (
 )
 
 REM Extract parameters and remove quotes
-set NAME=%~1
+set ASSEMBLY_FILENAME=%~1
 set SOURCE=%~2
 set TORCH=%~3
-set PROJECT=%~4
+set PLUGIN_NAME=%~4
 
 REM Remove trailing backslash if applicable
-if "%NAME:~-1%"=="\" set NAME=%NAME:~0,-1%
+if "%PLUGIN_NAME:~-1%"=="\" set PLUGIN_NAME=%PLUGIN_NAME:~0,-1%
 if "%SOURCE:~-1%"=="\" set SOURCE=%SOURCE:~0,-1%
 if "%TORCH:~-1%"=="\" set TORCH=%TORCH:~0,-1%
-if "%PROJECT:~-1%"=="\" set PROJECT=%PROJECT:~0,-1%
 
-REM Get the plugin directory
-set COMMON_PLUGIN_DIR=%TORCH%\Plugins
-
-REM Create this directory if it does not exist
-if not exist "%COMMON_PLUGIN_DIR%" (
-    echo Creating "Plugins\" folder in "%TORCH%\"
-    mkdir -p "%COMMON_PLUGIN_DIR%" >NUL 2>&1
-)
-
-REM Get the specific plugin directory
-set PLUGIN_DIR=%COMMON_PLUGIN_DIR%\%PROJECT%
+REM Get the plugin directory (subdirectory under Plugins with solution name)
+set PLUGIN_DIR=%TORCH%\Plugins\%PLUGIN_NAME%
 
 REM Create this directory if it does not exist
 if not exist "%PLUGIN_DIR%" (
-    echo Creating "%PROJECT%\" folder in "%COMMON_PLUGIN_DIR%\"
-    mkdir -p "%PLUGIN_DIR%" >NUL 2>&1
+    echo Creating "Plugins\%PLUGIN_NAME%\" folder in "%TORCH%\"
+    mkdir "%PLUGIN_DIR%" >NUL 2>&1
 )
 
-REM Copy the plugin into the plugin directory
-echo Copying "%NAME%" to "%PLUGIN_DIR%\"
+REM Copy the plugin DLL into the plugin directory
+echo Copying "%ASSEMBLY_FILENAME%" to "%PLUGIN_DIR%\"
 
 for /l %%i in (1, 1, 10) do (
-    copy /y "%SOURCE%\%NAME%" "%PLUGIN_DIR%\"
+    copy /y "%SOURCE%\%ASSEMBLY_FILENAME%" "%PLUGIN_DIR%\"
 
     if !ERRORLEVEL! NEQ 0 (
         REM "timeout" requires input redirection which is not supported,
@@ -55,10 +45,28 @@ for /l %%i in (1, 1, 10) do (
 
 REM This part will only be reached if the loop has been exhausted
 REM Any success would skip to the BREAK_LOOP_PLUGIN label below
-echo ERROR: Could not copy "%NAME%".
+echo ERROR: Could not copy "%ASSEMBLY_FILENAME%".
 exit /b 1
 
 :BREAK_LOOP_PLUGIN
+
+REM Copy manifest.xml into the plugin directory
+echo Copying "manifest.xml" to "%PLUGIN_DIR%\"
+
+for /l %%i in (1, 1, 10) do (
+    copy /y "%SOURCE%\manifest.xml" "%PLUGIN_DIR%\"
+
+    if !ERRORLEVEL! NEQ 0 (
+        ping -n 2 127.0.0.1 >NUL 2>&1
+    ) else (
+        goto BREAK_LOOP_MANIFEST
+    )
+)
+
+echo ERROR: Could not copy "manifest.xml".
+exit /b 1
+
+:BREAK_LOOP_MANIFEST
 
 REM Copy Harmony into the plugin directory
 echo Copying "0Harmony.dll" to "%PLUGIN_DIR%\"
@@ -67,40 +75,14 @@ for /l %%i in (1, 1, 10) do (
     copy /y "%SOURCE%\0Harmony.dll" "%PLUGIN_DIR%\"
 
     if !ERRORLEVEL! NEQ 0 (
-        REM "timeout" requires input redirection which is not supported,
-        REM so we use ping as a way to delay the script between retries.
         ping -n 2 127.0.0.1 >NUL 2>&1
     ) else (
         goto BREAK_LOOP_HARMONY
     )
 )
 
-REM This part will only be reached if the loop has been exhausted
-REM Any success would skip to the BREAK_LOOP_HARMONY label below
 echo ERROR: Could not copy "0Harmony.dll".
 exit /b 1
 
 :BREAK_LOOP_HARMONY
-
-REM Copy the Manifest into the plugin directory
-echo Copying "manifest.xml" to "%PLUGIN_DIR%\"
-
-for /l %%i in (1, 1, 10) do (
-    copy /y "%SOURCE%\manifest.xml" "%PLUGIN_DIR%\"
-
-    if !ERRORLEVEL! NEQ 0 (
-        REM "timeout" requires input redirection which is not supported,
-        REM so we use ping as a way to delay the script between retries.
-        ping -n 2 127.0.0.1 >NUL 2>&1
-    ) else (
-        goto BREAK_LOOP_MANIFEST
-    )
-)
-
-REM This part will only be reached if the loop has been exhausted
-REM Any success would skip to the BREAK_LOOP_MANIFEST label below
-echo ERROR: Could not copy "manifest.xml".
-exit /b 1
-
-:BREAK_LOOP_MANIFEST
 exit /b 0
